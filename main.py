70ec53e446623955d19c7dee40a461495ac3083b
val <ahahhaha@example.com>
2024-07-09 17:50:24 +0800

    У клиента в строке есть текст из браузера

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-import aiohttp
-import asyncio
+from aiohttp import web
 
-async def main():
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+async def poster(request):
+    d = str(request.match_info)
+    d2 = await request.post()
+    d3 = d2.get('message')
+    return web.Response(text=d + str(d3))
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://python.org') as response:
 
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.post('/{name}', poster),
+                web.get('/p', poster)])
 
-            html = await response.text()
-            print("Body:", html[:15], "...")
-
-asyncio.run(main())
+if __name__ == '__main__':
+    web.run_app(app, port=8000)
