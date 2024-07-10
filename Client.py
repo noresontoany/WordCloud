c81a3f3ddfe89ea4376f1032d132d63a45da4241
val <ahahhaha@example.com>
2024-07-11 05:36:09 +0800

    Получилось включить js

--- Client.py
+++ Client.py
@@ -1,12 +1,17 @@
 import aiohttp
 import asyncio
 
+client_names = []
 async def send_message():
     async with aiohttp.ClientSession() as session:
+        name = input("name->")
+        client_names.append(name)
         while True:
             message = input("Enter message: ")
             async with session.post('http://localhost:8000/p', data={'message': message}) as resp:
                 print(await resp.text())
+            async with session.post('http://localhost:8000/p') as resp:
+                pass
 
 if __name__ == '__main__':
     asyncio.run(send_message())
--- main.py
+++ main.py
@@ -1,24 +1,36 @@
+import time
+
+import aiohttp_jinja2
+import jinja2
 from aiohttp import web
 
-
 messages = []
-
+_old_size = len(messages)
+@aiohttp_jinja2.template('index.html')
+async def index(request):
+    return {'messages': messages}
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
-    return web.Response(text=text)
+    return web.Response(text=str(messages))
+
 async def poster(request):
     d = str(request.match_info)
     d2 = await request.post()
     d3 = d2.get('message')
-    messages.append(d3)
+    if d3 is not None:
+        messages.append(d3)
     return web.Response(text=d + str(messages))
 
 
-app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.post('/{name}', poster),
-                web.get('/p', poster)])
 
+
+app = web.Application()
+app.add_routes([web.get('/', index),
+                web.post('/p', poster),
+                web.get('/p', handle)])
+app.router.add_static('/static', path='static', name='static')
+
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('.'))
 if __name__ == '__main__':
     web.run_app(app, port=8000)
