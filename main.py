321f2428bcf62ba93970e28b59abcff9f1685134
val <ahahhaha@example.com>
2024-07-19 21:57:50 +0800

    передалал запрос каждый 5 сек, на веб соккет

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
-import time
-
+import aiohttp
 import aiohttp_jinja2
 import jinja2
 from aiohttp import web
-import random
+
 
 all_messages = []
-messages = []
-
+clients = []
 @aiohttp_jinja2.template('index.html')
 async def index(request):
     pass
-async def poster(request):
-    answer = await request.post()
-    message = answer.get('message')
-    if message is not None:
-        messages.append(message)
-        all_messages.append(message)
-    return web.Response()
-
-async def get_chart_data(request):
-    chart_data = []
-    for message in messages:
-        chart_data.append({"category": message, "value": 2.1})
-    messages.clear()
-    return web.json_response(chart_data)
-
 async def get_all_data(request):
     chart_data = []
     for message in all_messages:
         chart_data.append({"category": message, "value": 2.1})
     return web.json_response(chart_data)
 
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    clients.append(ws)
+    try:
+        async for msg in ws:
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                chart_data = {"category": msg.data, "value": 2.1}
+                all_messages.append(msg.data)
+                for client in clients:
+                    await client.send_json(chart_data)
+    finally:
+        clients.remove(ws)
+    return ws
+
+
 
 app = web.Application()
 app.add_routes([web.get('/', index),
-                web.post('/poster', poster),
-                web.get('/get-all-data', get_all_data),
-                web.get('/chart-data', get_chart_data)])
+                web.get('/ws', websocket_handler),
+                web.get('/get-all-data', get_all_data)])
 
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('public'))
 if __name__ == '__main__':
